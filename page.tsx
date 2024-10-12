bc39a252561a041516cadc5457030da44e034f37
alizahfayyaz22 <alizahfayyaz22@gmail.com>
2024-10-13 00:00:02 +0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,56 @@
+
+ 
+
+export default  function responsive(){
+  return (
+    <body>
+      <div className="parentcontainer">
+        <div className="childcontainer1">
+          <div className="imagecontainer"><img src="https://goldarrowcamp.com/app/uploads/2023/02/Kindness-1024x1024.png"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription"> Acts of kindness not only make others feel good but boost the mood of the giver as well .try make someone happy today because by this we can praise ALLAH too.
+             </p>
+             
+</div>
+            
+        <div className="childcontainer2">
+          <div className="imagecontainer"><img src="https://m.media-amazon.com/images/I/81Pz-P+VxlL.jpg"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription"> instead of going regular walks go on a "Sensory Walk" . Challenge is to notice five things you can see, four you can touch, three you can hear.
+             </p>
+             </div>
+             <div className="childcontainer3"> 
+          <div className="imagecontainer"><img src="https://media.assettype.com/freepressjournal/2023-01/7b81300f-ea90-40e8-847f-84fb8d77057b/fvc.jpg"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription"> draw whatever comes to mind without any judgment—this creative expression can help release tension and improve mood.
+             </p>
+             </div>
+             <div className="childcontainer4">
+          <div className="imagecontainer"><img src="https://media.simplemindfulness.com/wp-content/uploads/2018/02/gratitude-changes-everything.jpeg"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription">Find a peaceful time during your day to reflect on the positive things in your life.Note down three things you're grateful for, bringing a sense of positivity.
+             </p>
+             </div>
+             <div className="childcontainer5">
+          <div className="imagecontainer"><img src="https://i0.wp.com/dayoneapp.com/wp-content/uploads/2023/06/emotinoal-journaling.jpg?fit=2000%2C1335&quality=89&ssl=1"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription"> Try Journaling
+          Write about your thoughts or feelings for just a few minutes. Journaling helps release emotions and can make you feel lighter and more focused.
+             </p></div>
+             <div className="childcontainer6">
+          <div className="imagecontainer"><img src="https://jasonbybee.com/wp-content/uploads/2024/04/consciousness-meditation-neurosicnce.jpg"></img></div>
+          {/* <h1 className="tittle">blog tittle</h1> */}
+          <p className="discription">Do a 5-Minute Meditation
+Close your eyes, focus on your breath, and let go of any stress. This short mindfulness practice can reset your mind and boost calmness.
+
+
+             </p></div>
+         
+    <div className="tittle"><h1><header>FEEL BETTER FAST:"6 SIMPLE WAYS TO BRIGHTEN YOUR DAY"</header></h1></div>
+    <div className="ending"><h1><footer>CREATED BY:ALIZAH FAYYAZ</footer></h1></div>
+</div>
+</body>
+  
+  )
+}
+
